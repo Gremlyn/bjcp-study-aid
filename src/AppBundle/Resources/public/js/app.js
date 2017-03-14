6873dd942c82018f5600d435f6f502426541c856
Gremlyn <gremlyn1@gmail.com>
2017-03-14 10:28:24 -0400

    Retain the header removal query string as needed.

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -1,3 +1,14 @@
+function refreshPage() {
+    var parts = window.location.href.split('?');
+    var query = '';
+
+    if (parts[1] !== undefined && parts[1].indexOf('header=remove') > -1) {
+        query = '?header=remove';
+    }
+
+    window.location = parts[0] + query;
+}
+
 $('#show-fact').click(function (e) {
     e.preventDefault();
 
@@ -13,9 +24,9 @@ $('#hide-fact').click(function (e) {
 });
 
 $('.card-refresh').click(function (e) {
-    window.location = window.location.href.split("?")[0];
+    refreshPage();
 });
 
 $('.card-refresh-mobile').click(function (e) {
-    window.location = window.location.href.split("?")[0];
+    refreshPage();
 });
